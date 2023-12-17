aa17e5636df4c82ef90e646a2668de8f2d05c92b
Microsoft <penguin.o@>
2023-12-17 17:00:59 +0700

    Merge remote-tracking branch 'origin/main'

--- lesson11/main.py
+++ lesson11/main.py
@@ -1,17 +1,22 @@
 import sqlite3
 from pprint import pprint
 
-def get_films_age_rating():
+def get_films_by_genres(genre):
     conection = sqlite3.connect('films.db')
     cursor = conection.cursor()
     SQL = f"""
-        SELECT title, age_rating, year from movies
-        where age_rating >= 16
+        SELECT id from genres where name = '{genre}'
+    """
+    query = cursor.execute(SQL)
+    data = query.fetchone()
+
+    SQL = f"""
+        SELECT * from movies
+        where genre_id = {data[0]}
     """
     query = cursor.execute(SQL)
     data = query.fetchall()
-
     conection.close()
     return data
 
-pprint(get_films_age_rating())
+pprint(get_films_by_genres('фантастика'))
