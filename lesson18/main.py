b50b929307c115fa2325ae3e6120d2e89527cfb7
Microsoft <penguin.o@>
2024-03-03 15:58:23 +0700

    Merge remote-tracking branch 'origin/main'

--- lesson18/main.py
+++ lesson18/main.py
@@ -2,10 +2,32 @@ from flask import  Flask
 
 app = Flask(__name__)
 
+films_list = [
+    'Побег из Шоушенка',
+    'Зеленая миля',
+    'Властелин колец: Братство кольца',
+    'Интерстеллар',
+    'Звёздные войны: Эпизод V - Империя наносит ответный удар',
+    'В джазе только девушки',
+    'Шерлок Холмс',
+    'Парк Юрского периода',
+    'Американская история Х',
+    'Гарри Поттер и философский камень',
+]
+
 @app.route('/')
-@app.route('/home')
 def home_page():
-    return '<p>Hello, World!</p>'
+    films_view = []
+    for i, film_name in enumerate(films_list):
+        films_view.append(f'<a href="/film/{i}">{film_name}</a>')
+    return f'<br>'.join(films_view)
+
+@app.route('/film/<int:film_id>')
+def film_page(film_id):
+    try:
+        return f'название фильма: {films_list[film_id]}'
+    except IndexError:
+        return 'Фильм не найден'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
